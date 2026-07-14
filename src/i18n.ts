8503d6aa83c56d9c757436ed1e9892c22e4e7420
Abiram-k <abiramk0107@gmail.com>
2026-07-14 10:56:17 +0530

    chore: update default i18n language from Malayalam to English

--- src/i18n.ts
+++ src/i18n.ts
@@ -17,7 +17,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'ml',
+    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // react already safes from xss
